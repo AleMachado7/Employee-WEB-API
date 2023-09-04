9c8da34193baec8dcdf062321ae21bdd8f4683ec
Alexandre Machado <aleaxe7@gmail.com>
2023-09-04 19:39:20 -0300

    Update EmployeeModel

--- EmployeeWebApi/Models/EmployeeModel.cs
+++ EmployeeWebApi/Models/EmployeeModel.cs
@@ -6,11 +6,11 @@ namespace EmployeeWebApi.Models
     public class EmployeeModel
     {
         [Key]
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = new Guid();
         public string Name { get; set; }
         public string Surname { get; set; }
         public DepartmentEnum Department { get; set; }
-        public bool Active { get; set; }
+        public bool Active { get; set; } = true;
         public WorkShiftEnum WorkShift { get; set; }
         public DateTime CreationDate { get; set; } = DateTime.Now.ToLocalTime();
         public DateTime UpdateDate { get; set; }
