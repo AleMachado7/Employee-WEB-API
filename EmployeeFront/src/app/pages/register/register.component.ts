a38e12b694c0576b672b792137e7aef83256573b
Alexandre Machado <aleaxe7@gmail.com>
2023-12-12 20:24:09 -0300

    Add register component logic to allow user registration

--- EmployeeFront/src/app/pages/register/register.component.ts
+++ EmployeeFront/src/app/pages/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserParams } from 'src/app/models/User/UserParams';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-register',
@@ -10,9 +12,15 @@ export class RegisterComponent {
   formTitle: string = 'Register';
   btnAction: string = 'New Account';
 
-  constructor() {}
+  constructor(private userService: UserService, private router: Router) {}
 
   register(user: UserParams) {
-
+    this.userService.register(user).subscribe((data) => {
+      if (data.success) {
+        this.router.navigate(['login']);
+      } else {
+        alert(data.message);
+      }
+    });
   }
 }
